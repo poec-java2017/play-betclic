560e4e879b34243f5857376061e92d00d9d4c756
Fleuret-F <fleuret.florian@laposte.net>
2017-03-03 17:01:51 +0100

    ajout acount dans User et début de la méthode bet

--- app/controllers/BetManager.java
+++ app/controllers/BetManager.java
@@ -1,8 +1,10 @@
 package controllers;
 
-import models.Event;
+import models.*;
+import play.data.validation.Required;
 import play.mvc.Controller;
 
+import java.util.Date;
 import java.util.List;
 
 public class BetManager extends Controller {
@@ -11,6 +13,26 @@ public class BetManager extends Controller {
         List<Event> events = Event.find("resultHost is null").fetch();
 //        List<Event> events = Event.find("date > now()").fetch();
         renderJSON(events);
+    }
+
+    public static void bet(@Required String idEvent,
+                           @Required Short betChoice,
+                           @Required Float betAmount,
+                           @Required Float betQuotation) {
+
+        // TODO : récupérer Utilisateur connecté
+        User user = new User();
+        // TODO : pas encore fait
+        if (betAmount > user.acount){
+
+        }
+
+        Date actionDate = new Date();
+        OperationType operationType = new OperationType("BET");
+        Operation betOperation = new Operation(betAmount, actionDate, operationType, user);
+        Event event = Event.find("uniq = ?1", idEvent).first();
+
+        Bet bet = new Bet(actionDate, betQuotation, betChoice, user, event, betOperation);
     }
 
 }
--- app/models/User.java
+++ app/models/User.java
@@ -35,6 +35,8 @@ public class User extends Model {
 
     public String telephone;
 
+    public Float acount;
+
     @ManyToOne
     public Address address;
 

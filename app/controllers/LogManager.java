459a20c85f137286f5462d409ca2ededb50ad16e
Anthony Periquet <xylphid>
2017-03-06 14:49:36 +0100

    Pass connected user to render

--- app/controllers/LogManager.java
+++ app/controllers/LogManager.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.User;
 import play.Logger;
 import play.mvc.After;
 import play.mvc.Before;
@@ -7,13 +8,22 @@ import play.mvc.Controller;
 
 public class LogManager extends Controller {
 
+    public static final String PREFIX = "LogController";
+
     @Before
     private void before() {
-        Logger.info(request.url);
+        Logger.info("[%s][before] URL : %s", PREFIX, request.url);
+
+        // Get user if connected
+        if (session.contains("username")) {
+            Logger.info("[%s][before] %s", PREFIX, session.get("username"));
+            User user = User.find("email = ?1", session.get("username")).first();
+
+            renderArgs.put("user", user);
+        }
     }
 
     @After
     private void after() {
-        Logger.info("------------------------------");
     }
 }

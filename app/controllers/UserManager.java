b41c72131a6850df2b5e1f0a2d6007590e00003d
Anthony Periquet <xylphid>
2017-03-07 07:12:34 +0100

    Add anchor to callback url for updatePassword, Add JS automating action to render correct form

--- app/controllers/UserManager.java
+++ app/controllers/UserManager.java
@@ -8,6 +8,7 @@ import play.data.validation.Equals;
 import play.data.validation.Error;
 import play.data.validation.Required;
 import play.data.validation.Validation;
+import play.mvc.Router;
 import play.mvc.With;
 import services.UserService;
 
@@ -20,13 +21,18 @@ public class UserManager extends LogManager {
     private static final String PREFIX = "User";
 
     public static void display() {
+        Logger.info("[%s][display]", PREFIX);
         List<Country> countries = Country.find("order by name").fetch();
 
         render(countries);
     }
 
     public static void createOrUpdate() {
-        display();
+        Logger.info("[%s][createOrUpdate]", PREFIX);
+
+        // Build URL to display form (JS required)
+        String url = Router.reverse("UserManager.display").addRef("address").url;
+        redirect(url);
     }
 
     public static void updatePassword(@Required String apppassword, @Required @Equals("apprnpassword") String appnpassword, @Required String apprnpassword) {
@@ -34,18 +40,20 @@ public class UserManager extends LogManager {
         User user = (User)renderArgs.get("user");
         if (Validation.hasErrors()) {
             for (Error error: Validation.errors()) {
-                Logger.info(error.message());
+                Logger.info("[%s][updatePassword][ValidationError] %s : %s", PREFIX, error.getKey(), error.message());
             }
             Validation.keep();
         } else if (!BCrypt.checkpw(apppassword, user.password)) {
-            Logger.info("[%s][updatePassword] Password mismatch", PREFIX);
+            Logger.info("[%s][updatePassword][BusinessError] Password mismatch", PREFIX);
             flash.put("badPassword", "Password mismatch.");
         } else {
             user.password = BCrypt.hashpw(appnpassword, BCrypt.gensalt());
             user.save();
         }
 
-        display();
+        // Build URL to display form (JS required)
+        String url = Router.reverse("UserManager.display").addRef("password").url;
+        redirect(url);
     }
 
     public static void signOut(@Valid User user){

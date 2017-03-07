31eb71746b36e925b90b3f48d8dbfef67c3038c2
Anthony Periquet <xylphid>
2017-03-07 22:34:07 +0100

    Add withdraw api, Check account before bet or withdraw, Display correct operation type in account resume

--- app/controllers/api/OperationManager.java
+++ app/controllers/api/OperationManager.java
@@ -1,6 +1,8 @@
 package controllers.api;
 
 import controllers.api.exception.BadCredentials;
+import controllers.api.exception.Business;
+import controllers.api.exception.NoContent;
 import controllers.service.OperationTypeService;
 import models.Operation;
 import models.OperationType;
@@ -9,6 +11,7 @@ import play.Logger;
 import play.data.validation.Min;
 import play.data.validation.Required;
 import play.data.validation.Validation;
+import services.UserService;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -54,8 +57,41 @@ public class OperationManager extends ApiManager {
             Logger.info("[%s][bet] Bad credentials with token [%s]", PREFIX, userToken);
             throw new BadCredentials();
         }
+        if (amount.compareTo(UserService.account(user.id)) > 0) {
+            Logger.info("[%s][bet] Tried to bet [%s] but not enougth credits.", PREFIX, amount);
+            throw new Business("Not enougth credits.");
+        }
         // Get operation.type.refill operation type
         OperationType type = OperationTypeService.findOrCreate("operation.type.bet");
+
+        Operation operation = new Operation();
+        operation.user = user;
+        operation.operationType = type;
+        operation.date = new Date();
+        operation.amount = amount.negate();
+        operation.save();
+
+        apiContentCreated(operation);
+    }
+
+    public static void withdraw(@Required String userToken, @Required @Min(0.01) BigDecimal amount) {
+        amount = amount.abs();
+        if (Validation.hasErrors()) {
+            apiBadInput(Validation.errors());
+        }
+
+        // Check user
+        User user = User.find("uniq = ?1", userToken).first();
+        if (user == null) {
+            Logger.info("[%s][bet] Bad credentials with token [%s]", PREFIX, userToken);
+            throw new BadCredentials();
+        }
+        if (amount.compareTo(UserService.account(user.id)) > 0) {
+            Logger.info("[%s][withdraw] Tried to withdraw [%s] but not enougth credits.", PREFIX, amount);
+            throw new Business("Not enougth credits.");
+        }
+        // Get operation.type.refill operation type
+        OperationType type = OperationTypeService.findOrCreate("operation.type.withdraw");
 
         Operation operation = new Operation();
         operation.user = user;

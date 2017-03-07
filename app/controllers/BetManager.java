e9f9fc118ce26b12ef62ea9d6f0858fe8bf9e803
Anthony Periquet <xylphid>
2017-03-07 15:55:04 +0100

    Move to BigDecimal amounts

--- app/controllers/BetManager.java
+++ app/controllers/BetManager.java
@@ -8,11 +8,12 @@ import play.mvc.Controller;
 import play.mvc.With;
 import services.UserService;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
 @With(SecureManager.class)
-public class BetManager extends Controller {
+public class BetManager extends LogManager {
 
     public static void eventsToBet() {
         List<Event> events = Event.find("resultHost is null").fetch();
@@ -22,7 +23,7 @@ public class BetManager extends Controller {
 
     public static void bet(@Required String idEvent,
                            @Required Short betChoice,
-                           @Required @Min(0.01) Float betAmount,
+                           @Required @Min(0.01) BigDecimal betAmount,
                            @Required Float betQuotation) {
         // TODO Affiner la validation du betChoice
 
@@ -30,13 +31,13 @@ public class BetManager extends Controller {
         // L'opération suivante permet de tester avant d'avoir le formulaire de créditation du compte, wesh
         // TODO Virer cette opération
         OperationType operationType1 = new OperationType("BET");
-        Operation operation = new Operation((float) 50, new Date(), operationType1, user);
+        Operation operation = new Operation(BigDecimal.valueOf(50), new Date(), operationType1, user);
         operationType1.save();
         operation.save();
         // TODO fin
 
-        Float solde = UserService.account(25L);
-        if (betAmount > solde){
+        BigDecimal solde = UserService.account(25L);
+        if (betAmount.compareTo(solde) >= 0) {
             Validation.addError("amount", "Votre solde n'est pas suffisant.");
         }
 
@@ -49,7 +50,7 @@ public class BetManager extends Controller {
         //Logger.info("Solde suffisant");
         Date actionDate = new Date();
         OperationType operationType = new OperationType("BET");
-        Operation betOperation = new Operation(betAmount * -1, actionDate, operationType, user);
+        Operation betOperation = new Operation(betAmount.negate(), actionDate, operationType, user);
         Event event = Event.find("uniq = ?1", idEvent).first();
         notFoundIfNull(event);
         Bet bet = new Bet(actionDate, betQuotation, betChoice, user, event, betOperation);
--- app/models/Operation.java
+++ app/models/Operation.java
@@ -6,6 +6,7 @@ import play.db.jpa.Model;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.UUID;
 
@@ -15,19 +16,22 @@ public class Operation extends Model {
     public String uniq;
     @Required
     @Min(0.01)
-    public Float amount;
+    public BigDecimal amount;
+
     @Required
     public Date date;
+
     @ManyToOne
     @Required
     public OperationType operationType;
+
     @Required
     @ManyToOne
     public User user;
 
     public Operation() { uniq = UUID.randomUUID().toString(); }
 
-    public Operation(Float amount, Date date, OperationType operationType, User user) {
+    public Operation(BigDecimal amount, Date date, OperationType operationType, User user) {
         uniq = UUID.randomUUID().toString();
         this.amount = amount;
         this.date = date;

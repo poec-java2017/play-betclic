ad7f34f593da92181b9a13bc2a663715ae281f07
Jean-Baptiste Soyez <enknower@gmail.com>
2017-03-08 12:09:14 +0100

    refactoring et corrections de la création de bets

--- app/controllers/BetManager.java
+++ app/controllers/BetManager.java
@@ -6,6 +6,7 @@ import play.data.validation.Required;
 import play.data.validation.Validation;
 import play.mvc.Controller;
 import play.mvc.With;
+import services.OperationTypeService;
 import services.UserService;
 
 import java.math.BigDecimal;
@@ -29,14 +30,9 @@ public class BetManager extends LogManager {
 
         User user = getConnectedUser();
         // L'opération suivante permet de tester avant d'avoir le formulaire de créditation du compte, wesh
-        // TODO Virer cette opération
-        OperationType operationType1 = new OperationType("BET");
-        Operation operation = new Operation(BigDecimal.valueOf(50), new Date(), operationType1, user);
-        operationType1.save();
-        operation.save();
         // TODO fin
 
-        BigDecimal solde = UserService.account(25L);
+        BigDecimal solde = UserService.account(user.id);
         if (betAmount.compareTo(solde) >= 0) {
             Validation.addError("amount", "Votre solde n'est pas suffisant.");
         }
@@ -49,12 +45,11 @@ public class BetManager extends LogManager {
 
         //Logger.info("Solde suffisant");
         Date actionDate = new Date();
-        OperationType operationType = new OperationType("BET");
+        OperationType operationType = OperationTypeService.getBet();
         Operation betOperation = new Operation(betAmount.negate(), actionDate, operationType, user);
         Event event = Event.find("uniq = ?1", idEvent).first();
         notFoundIfNull(event);
         Bet bet = new Bet(actionDate, betQuotation, betChoice, user, event, betOperation);
-        operationType.save();
         betOperation.save();
         bet.save();
         EventManager.events();

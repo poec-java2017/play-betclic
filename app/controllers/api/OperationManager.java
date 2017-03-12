5118c3c5bf0174740643df90367a36bcdf0626d0
Anthony Periquet <xylphid>
2017-03-12 17:07:15 +0100

    Refactor api-operation

--- app/controllers/api/OperationManager.java
+++ app/controllers/api/OperationManager.java
@@ -2,18 +2,16 @@ package controllers.api;
 
 import controllers.api.exception.BadCredentials;
 import controllers.api.exception.Business;
-import controllers.service.OperationTypeService;
 import models.Operation;
-import models.OperationType;
 import models.User;
 import play.Logger;
 import play.data.validation.Min;
 import play.data.validation.Required;
 import play.data.validation.Validation;
+import services.OperationService;
 import services.UserService;
 
 import java.math.BigDecimal;
-import java.util.Date;
 
 public class OperationManager extends ApiManager {
 
@@ -36,15 +34,8 @@ public class OperationManager extends ApiManager {
             Logger.info("[%s][refill] Bad credentials with token [%s]", PREFIX, userToken);
             throw new BadCredentials();
         }
-        // Get operation.type.refill operation type
-        OperationType type = OperationTypeService.findOrCreate("operation.type.refill");
-
-        Operation operation = new Operation();
-        operation.user = user;
-        operation.type = type;
-        operation.date = new Date();
-        operation.amount = amount;
-        operation.save();
+        // Create operation (cascade to operationType if needed)
+        Operation operation = OperationService.createOperation(user, "operation.type.refill", amount);
 
         apiContentCreated(operation);
     }
@@ -66,25 +57,18 @@ public class OperationManager extends ApiManager {
             Logger.info("[%s][bet] Bad credentials with token [%s]", PREFIX, userToken);
             throw new BadCredentials();
         }
-        if (amount.compareTo(UserService.account(user.id)) > 0) {
+        if (amount.compareTo(UserService.getAccountBalance(user.id)) > 0) {
             Logger.info("[%s][bet] Tried to bet [%s] but not enougth credits.", PREFIX, amount);
             throw new Business("Not enougth credits.");
         }
-        // Get operation.type.refill operation type
-        OperationType type = OperationTypeService.findOrCreate("operation.type.bet");
-
-        Operation operation = new Operation();
-        operation.user = user;
-        operation.type = type;
-        operation.date = new Date();
-        operation.amount = amount.negate();
-        operation.save();
+        // Create operation (cascade to operationType if needed)
+        Operation operation = OperationService.createOperation(user, "operation.type.bet", amount.negate());
 
         apiContentCreated(operation);
     }
 
     /**
-     * Withdraw to user bank account
+     * Withdraw to user bank getAccountBalance
      * @param userToken User uniq id
      * @param amount Withdrawal amount
      */
@@ -100,19 +84,13 @@ public class OperationManager extends ApiManager {
             Logger.info("[%s][bet] Bad credentials with token [%s]", PREFIX, userToken);
             throw new BadCredentials();
         }
-        if (amount.compareTo(UserService.account(user.id)) > 0) {
+        // Compare getAccountBalance balance to withdrawal amount
+        if (amount.compareTo(UserService.getAccountBalance(user.id)) > 0) {
             Logger.info("[%s][withdraw] Tried to withdraw [%s] but not enougth credits.", PREFIX, amount);
             throw new Business("Not enougth credits.");
         }
-        // Get operation.type.refill operation type
-        OperationType type = OperationTypeService.findOrCreate("operation.type.withdraw");
-
-        Operation operation = new Operation();
-        operation.user = user;
-        operation.type = type;
-        operation.date = new Date();
-        operation.amount = amount.negate();
-        operation.save();
+        // Create operation (cascade to operationType if needed)
+        Operation operation = OperationService.createOperation(user, "operation.type.withdraw", amount.negate());
 
         apiContentCreated(operation);
     }
